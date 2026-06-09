8c8dead06ef54e58c102f7f068dc41db92065cde
nando <ham280207@gmail.com>
2026-06-10 03:16:10 +0700

    feat: dashboard particle

--- src/view/pages/dashboard/DashboardPage.jsx
+++ src/view/pages/dashboard/DashboardPage.jsx
@@ -2,6 +2,7 @@ import AboutPage from "../about/AboutPage";
 import GalleryPage from "../gallery/GalleryPage";
 import ExperiencePage from "../experiences/ExperiencePage";
 import MoreAboutMe from "../moreinfo/MoreAboutMe";
+import AboutParticle from "../../components/particle/AboutParticle";
 
 const DashboardPage = () => {
     const scrollToAbout = () => {
@@ -14,6 +15,7 @@ const DashboardPage = () => {
     return (
         <div className="flex flex-col">
             {/* Hero Section */}
+            <AboutParticle/>
             <div className="h-[calc(100vh-100px)] w-full flex flex-col items-center justify-center overflow-hidden relative">
                 <div className="relative z-10 flex flex-col items-center">
                     <div className="mb-12" data-aos="fade-up">
